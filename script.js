25d184e08a3627695c3f6a6141b7eb7259e20484
darkxel2004 <armandocdelcastillo2004@gmail.com>
2026-01-27 20:24:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+window.onload = () => {
+  document.getElementById("transicion").style.display = "none";
+};
+
 document.getElementById("btnEntrar").addEventListener("click", () => {
   const usuario = document.getElementById("usuario").value.trim().toLowerCase();
   const clave = document.getElementById("clave").value.trim();
@@ -5,11 +9,19 @@ document.getElementById("btnEntrar").addEventListener("click", () => {
   if (usuario === "dark" && clave === "2026") {
     localStorage.setItem("logueado", "true");
 
-    document.getElementById("transicion").style.display = "flex";
+    document.getElementById("loginBox").style.display = "none";
+    document.getElementById("titulo").style.display = "none";
+
+    const transicion = document.getElementById("transicion");
+    const video = document.getElementById("videoCarga");
+
+    transicion.style.display = "flex";
+    video.play();
 
     setTimeout(() => {
       window.location.href = "menu.html";
     }, 3500);
+
   } else {
     alert("❌ Usuario o contraseña incorrectos");
   }
